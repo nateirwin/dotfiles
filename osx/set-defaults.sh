0ae25ce6844b5f002c4ae7807302356a406c74b6
Nate Irwin <nate@nateirwin.net>
2012-11-26 11:00:43 -0700

    Add git and osx configs

---
+++ osx/set-defaults.sh
@@ -0,0 +1,4 @@
+# Sets my OS X defaults. Taken from https://github.com/holman/dotfiles/blob/master/osx/set-defaults.sh.
+
+# Open everything in Finder's list view.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
